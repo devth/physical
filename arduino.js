decdff57ac9c7d07792f7b28265372f906cce88e
Trevor Hartman <trevorhartman@gmail.com>
2013-11-17 16:20:55 -0700

    Use the pot to control speed and direction of motor

--- arduino.js
+++ arduino.js
@@ -1,68 +1,50 @@
 var five = require("johnny-five"),
     board = new five.Board();
 
-
-
 board.on("ready", function() {
 
-  var enablePin = new five.Pin(11),
-      in1 = new five.Pin(4),
+  var enablePin = 11;
+  this.pinMode(enablePin, five.Pin.PWM);
+  // var enablePin = new five.Pin(11),
+  var in1 = new five.Pin(4),
       in2 = new five.Pin(7),
-      forward = true;
+      isForward = true;
 
   var forward = function() {
     in1.high();
     in2.low();
   };
-
   var backward = function() {
-    in1.high();
-    in2.low();
+    in1.low();
+    in2.high();
   };
 
+  in1.high();
+  in2.low();
 
-  enablePin.high();
+  new five.Sensor("I0").scale(0, 255).on("change", function() {
+    var val = this.value;
+    var pinValue;
+    if (val < 128) {
+      backward();
+      pinValue = (255 - (val * 2));
+    } else {
+      forward();
+      pinValue = (val - 128) * 2;
+    }
 
-  // in1.high();
-  // in2.low();
-
-
-  // var pot = ;
-
-  new five.Sensor("I0").scale(0, 180).on("change", function() {
-    console.log(this.value);
+    console.log(this.value, pinValue);
+    board.analogWrite(enablePin, pinValue);
   });
 
-
-  board.loop(1000, function() {
-
-    // if (forward) {
+  // board.loop(1000, function() {
+    // if (isforward) {
     //   in1.low();
     //   in2.high();
     // } else {
     //   in1.high();
     //   in2.low();
     // }
-
-    forward = !forward;
-  });
-
+    // isforward = !isforward;
+  // });
 });
-
-
-
-// void loop()
-// {
-//   digitalWrite(in1A, HIGH);
-//   digitalWrite(in2A, LOW);
-//   digitalWrite(enablePin, HIGH);
-//   delay(5000);
-//   digitalWrite(enablePin, LOW);
-//   delay(2000);
-//   digitalWrite(in1A, LOW);
-//   digitalWrite(in2A, HIGH);
-//   digitalWrite(enablePin, HIGH);
-//   delay(5000);
-//   digitalWrite(enablePin, LOW);
-//   delay(2000);
-// }
